f11087c3f9075dde0d8600cbe3865ab42fad28cc
nngocjade <nngocjade@gmail.com>
2021-01-27 17:31:12 +0700

    added view for login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import PublicNavbar from "./component/PublicNavbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PublicNavbar />
+      <Switch>
+        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/login" component={LoginPage} />
+      </Switch>
     </div>
   );
 }
